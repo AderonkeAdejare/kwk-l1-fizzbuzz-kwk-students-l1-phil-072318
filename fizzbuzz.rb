08b3093657a825cc7cefc709da44bc2ba7988807
AderonkeAdejare <aderonkea@comcast.net>
2018-07-24 19:05:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(number)
   puts "What is your number?"
   number = gets.chomp.to_i 
-end 
-if number % 3 
-  puts "fizz"
-elsif number % 5
-  puts "buzz"
-elsif number % 3 && number % 5
+end
+if number % 3 == 0 && number % 5 == 0 
   puts "fizzbuzz".upcase
+elsif number % 3 == 0
+  puts "fizz"
+elsif number % 5 == 0 
+  puts "buzz"
 elsif 
   puts "Sorry, this number is not divisible by 3 or 5. Please try again. "
 end
